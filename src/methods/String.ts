5c23091fd304d3011aaed283fa7a0db3cdffd80e
Hugo Ferreira <hugoferreirait@gmail.com>
2022-05-21 19:22:43 -0300

    chore: enhancing string method

--- src/methods/String.ts
+++ src/methods/String.ts
@@ -1,11 +1,14 @@
 String.prototype.formatToSearch = function(string): boolean {
-  return this
-  .replace(/\s|_|\(|\)/g, "-")
+
+  const str = this.replace(/\s|_|\(|\)/g, "-")
   .normalize("NFD").replace(/\p{Diacritic}/gu, "")
   .toLowerCase()
-  .startsWith(string.replace(/\s|_|\(|\)/g, "-")
+
+  const search = string.replace(/\s|_|\(|\)/g, "-")
   .normalize("NFD").replace(/\p{Diacritic}/gu, "")
-  .toLowerCase())
+  .toLowerCase()
+
+  return str.startsWith(search) || str.includes(search)
 }
 
 export {}
